3c148273917d40a548adb6742845fa9a9312389b
agyeievans <evanskofiagyei06@gmail.com>
2023-05-06 14:30:32 +0000

    testing view

--- drf_project/views.py
+++ drf_project/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'username': 'John',
+            'age': 23
+        }
+        return Response(data)
