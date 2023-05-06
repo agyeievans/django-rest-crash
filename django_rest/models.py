801d7c752b46f0e87fb2c386052e9577d6a3ee34
agyeievans <evanskofiagyei06@gmail.com>
2023-05-06 14:41:54 +0000

    added student model

--- django_rest/models.py
+++ django_rest/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    decription = models.TextField()
+    date_enrolled = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
