39ba60621f4f776e5412d4d2863ed58c20bda37d
agyeievans <evanskofiagyei06@gmail.com>
2023-05-06 14:53:33 +0000

    created post function

--- drf_project/views.py
+++ drf_project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django_rest.serializers import StudentSerializer
+from django_rest.models import Student
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
@@ -9,3 +11,11 @@ class TestView(APIView):
             'age': 23
         }
         return Response(data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
